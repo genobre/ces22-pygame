362ff5cc808499195c97fea12e4e2d1e8b1b257f
gmrperdigao <36706143+gmrperdigao@users.noreply.github.com>
2018-04-26 23:28:10 -0300

    Update sprites.py
    Jogo completo (sem som)

--- ces22/jogo_pygame/code1/sprites.py
+++ ces22/jogo_pygame/code1/sprites.py
@@ -55,7 +55,6 @@ class Player(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = vec(16, HH)
 
-        self.pos = vec(16, HH)
         self.vel = velocity
         self.points = 0
 
@@ -108,11 +107,9 @@ class Player(pg.sprite.Sprite):
     def update(self):
         self.animate()
         if ((self.xVelocity > 0 and self.rect.right < (W - 15)) or (self.xVelocity < 0 and self.rect.left > 15)):
-            self.pos.x += self.xVelocity
-            self.rect.right = self.pos.x
+            self.rect.centerx += self.xVelocity
         if ((self.yVelocity > 0 and self.rect.bottom < (H - 15)) or (self.yVelocity < 0 and self.rect.top > 15)):
-            self.pos.y += self.yVelocity
-            self.rect.bottom = self.pos.y
+            self.rect.centery += self.yVelocity
 
     def animate(self):
         now = pg.time.get_ticks()
@@ -148,11 +145,14 @@ class PlasticBag(pg.sprite.Sprite):
         self.load_images()
         self.image = self.floating_frames[0]
         self.rect = self.image.get_rect()
-        self.rect.centerx = choice([-100, 3492])
         self.got_shot = False
+        self.rect.centerx = choice([-100, W + 100])
         self.rect.y = randrange(H)
         self.vy = 0
-        self.vx = 1
+        if self.rect.centerx < 0:
+            self.vx = 1
+        else:
+            self.vx = -1
         self.dy = 0.25
 
     def load_images(self):
@@ -176,10 +176,10 @@ class PlasticBag(pg.sprite.Sprite):
         self.rect.center = center
         self.rect.y += self.vy
         if self.got_shot:
-            self.rect.centerx += choice([-20, -10, 0, 10, 20])
+            self.rect.x += choice([-20, -10, 0, 10, 20])
             self.rect.centery += choice([-20, -10, 0, 10, 20])
             self.got_shot = False
-        if self.rect.left > 3492 or self.rect.right < -100:
+        if self.rect.left < -200 or self.rect.right > W + 200:
             self.kill()
         if self.rect.top < -10 or self.rect.bottom > 500:
             self.kill()
@@ -227,9 +227,9 @@ class Krill(pg.sprite.Sprite):
         self.load_images()
         self.image = self.floating_frames[0]
         self.rect = self.image.get_rect()
-        self.rect.centerx = (W - 30)
-        self.rect.centery = randint(15,H-15)
-        self.vx = -0.1
+        self.rect.centerx = randint(0,W)
+        self.rect.centery = randint(0,H)
+        self.vx = -0.5
 
     def load_images(self):
         self.floating_frames = [self.game.krillsprite.sprt(6, 1, 0 % 6),
@@ -270,10 +270,13 @@ class JellyFish(pg.sprite.Sprite):
         self.load_images()
         self.image = self.floating_frames[0]
         self.rect = self.image.get_rect()
-        self.rect.centerx = choice([-100, 3492])
+        self.rect.centerx = choice([-100, W + 100])
         self.rect.y = randrange(H)
         self.vy = 0
-        self.vx = 1
+        if self.rect.centerx < 0:
+            self.vx = 1
+        else:
+            self.vx = -1
         self.dy = 0.25
 
     def load_images(self):
@@ -296,7 +299,7 @@ class JellyFish(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = center
         self.rect.y += self.vy
-        if self.rect.left > 3492 or self.rect.right < -100:
+        if self.rect.left < -200 or self.rect.right > W + 200:
             self.kill()
         if self.rect.top < -10 or self.rect.bottom > 500:
             self.kill()
@@ -322,7 +325,7 @@ class Can(pg.sprite.Sprite):
         self.image = self.floating_frames[0]
         self.rect = self.image.get_rect()
         self.rect.centerx = (W - 30)
-        self.rect.centery = randint(15, 100)
+        self.rect.centery = randint(10, 420)
         self.vx = -0.1
         self.got_shot = False
 
@@ -412,9 +415,13 @@ class Fish(pg.sprite.Sprite):
         self.load_images()
         self.image = self.floating_frames[0]
         self.rect = self.image.get_rect()
-        self.rect.centerx = (W - 30)
-        self.rect.centery = randint(250, 400)
-        self.vx = -3
+        self.rect.centerx = choice([-100, W + 100])
+        self.rect.y = randrange(H)
+        self.vy = 0
+        if self.rect.centerx < 0:
+            self.vx = choice([1, 2, 3])
+        else:
+            self.vx = choice([1, -2, -3])
         self.got_shot = False
         self.life = 3
 
@@ -423,6 +430,10 @@ class Fish(pg.sprite.Sprite):
                                 self.game.fishsprite.sprt(2, 1, 1 % 2)]
         for frame in self.floating_frames:
             frame.set_colorkey(BLACK)
+        self.swim_l_frames = []
+        for frame in self.floating_frames:
+            frame.set_colorkey(BLACK)
+            self.swim_l_frames.append(pg.transform.flip(frame, True, False))
 
     def update(self):
         self.animate()
@@ -437,8 +448,6 @@ class Fish(pg.sprite.Sprite):
         self.rect.center = center
         if self.life <= 0:
             self.kill()
-        if self.rect.left > 800 or self.rect.right < -5:
-            self.kill()
 
     def animate(self):
         now = pg.time.get_ticks()
@@ -446,5 +455,175 @@ class Fish(pg.sprite.Sprite):
         if now - self.last_update > 180:
             self.last_update = now
             self.current_frame = (self.current_frame + 1) % len(self.floating_frames)
-            self.image = self.floating_frames[self.current_frame]
+            if self.vx <0:
+                self.image = self.floating_frames[self.current_frame]
+            else:
+                self.image = self.swim_l_frames[self.current_frame]
         self.mask = pg.mask.from_surface(self.image)
+
+
+class BossHead(pg.sprite.Sprite):
+    def __init__(self, game):
+        self.groups = game.all_sprites, game.heads
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.current_frame = 0
+        self.last_update = 0
+        self.load_images()
+        self.image = self.head_frames[0]
+        self.rect = self.image.get_rect()
+        self.rect.centerx = W - 200
+        self.rect.centery = 250
+        self.got_shot = False
+        self.life = 30
+        self.vx = 2
+
+    def load_images(self):
+        self.head_frames = [self.game.headsprite.sprt(5, 4, 0 % 20),
+                            self.game.headsprite.sprt(5, 4, 1 % 20),
+                            self.game.headsprite.sprt(5, 4, 2 % 20),
+                            self.game.headsprite.sprt(5, 4, 3 % 20),
+                            self.game.headsprite.sprt(5, 4, 4 % 20),
+                            self.game.headsprite.sprt(5, 4, 5 % 20),
+                            self.game.headsprite.sprt(5, 4, 6 % 20),
+                            self.game.headsprite.sprt(5, 4, 7 % 20),
+                            self.game.headsprite.sprt(5, 4, 8 % 20),
+                            self.game.headsprite.sprt(5, 4, 9 % 20),
+                            self.game.headsprite.sprt(5, 4, 10 % 20),
+                            self.game.headsprite.sprt(5, 4, 11 % 20),
+                            self.game.headsprite.sprt(5, 4, 12 % 20),
+                            self.game.headsprite.sprt(5, 4, 13 % 20),
+                            self.game.headsprite.sprt(5, 4, 14 % 20),
+                            self.game.headsprite.sprt(5, 4, 15 % 20),
+                            self.game.headsprite.sprt(5, 4, 16 % 20),
+                            self.game.headsprite.sprt(5, 4, 17 % 20),
+                            self.game.headsprite.sprt(5, 4, 18 % 20),
+                            self.game.headsprite.sprt(5, 4, 19 % 20)]
+        for frame in self.head_frames:
+            frame.set_colorkey(BLACK)
+
+        self.headhit_frames = [self.game.headhitsprite.sprt(5, 4, 0 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 1 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 2 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 3 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 4 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 5 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 6 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 7 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 8 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 9 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 10 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 11 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 12 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 13 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 14 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 15 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 16 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 17 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 18 % 20),
+                               self.game.headhitsprite.sprt(5, 4, 19 % 20)]
+        for frame in self.headhit_frames:
+            frame.set_colorkey(BLACK)
+
+    def update(self):
+        self.animate()
+        center = self.rect.center
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        if self.life <= 0:
+            self.rect.centerx += self.vx
+            self.rect.centery += self.vx
+            if self.rect.right > W + 200:
+                self.kill()
+
+    def animate(self):
+        now = pg.time.get_ticks()
+        # show swimming animation
+        if now - self.last_update > 180:
+            self.last_update = now
+            self.current_frame = (self.current_frame + 1) % len(self.head_frames)
+            if not self.got_shot:
+                self.rect.centerx = W - 200
+                self.image = self.head_frames[self.current_frame]
+                self.image = pg.transform.scale(self.image, (400, 400))
+            else:
+                self.image = self.headhit_frames[self.current_frame]
+                self.image = pg.transform.scale(self.image, (360, 360))
+                self.rect.centerx = W - 180
+                self.got_shot = False
+        self.mask = pg.mask.from_surface(self.image)
+
+
+class BossTentacles(pg.sprite.Sprite):
+    def __init__(self, game):
+        self.groups = game.all_sprites, game.tentacles
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.current_frame = 0
+        self.last_update = 0
+        self.load_images()
+        self.image = self.tentacle_frames[0]
+        self.rect = self.image.get_rect()
+        self.rect.centerx = W - 200
+        self.rect.centery = 250
+
+    def load_images(self):
+        self.tentacle_frames = [self.game.tentaclesprite.sprt(5, 4, 0 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 1 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 2 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 3 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 4 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 5 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 6 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 7 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 8 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 9 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 10 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 11 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 12 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 13 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 14 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 15 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 16 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 17 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 18 % 20),
+                            self.game.tentaclesprite.sprt(5, 4, 19 % 20)]
+        for frame in self.tentacle_frames:
+            frame.set_colorkey(BLACK)
+
+    def update(self):
+        self.animate()
+        center = self.rect.center
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        if self.rect.right > 1000:
+            self.kill()
+
+    def animate(self):
+        now = pg.time.get_ticks()
+        # show swimming animation
+        if now - self.last_update > 180:
+            self.last_update = now
+            self.current_frame = (self.current_frame + 1) % len(self.tentacle_frames)
+            self.image = self.tentacle_frames[self.current_frame]
+            self.image = pg.transform.scale(self.image, (400, 400))
+        self.mask = pg.mask.from_surface(self.image)
+
+
+class BossInk(pg.sprite.Sprite):
+    def __init__(self, game):
+        self.groups = game.all_sprites, game.inks
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = self.game.inksprite.sprt(1, 1, 0 % 1)
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.velocity = -5
+        self.rect.centerx = W - 350
+        self.rect.centery = randint(50, 450)
+        self.mask = pg.mask.from_surface(self.image)
+
+    def update(self):
+        self.rect.right += self.velocity
+        if self.rect.left < 0 or self.rect.right > 800:
+            self.kill()
